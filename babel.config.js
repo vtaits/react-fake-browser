0ea6cd22c170f609578bd2430e07cad9d41e4114
Vadim Taits <taizvadim@gmail.com>
2020-04-04 17:58:12 +0300

    Added boilerplate

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    es: {
+      presets: [
+        '@babel/preset-typescript',
+        [
+          '@babel/env', {
+            modules: false,
+          },
+        ],
+        '@babel/preset-react',
+      ],
+    },
+
+    cjs: {
+      presets: [
+        '@babel/preset-typescript',
+        '@babel/env',
+        '@babel/preset-react',
+      ],
+    },
+
+    test: {
+      presets: [
+        '@babel/preset-typescript',
+        '@babel/env',
+        '@babel/preset-react',
+      ],
+    },
+  },
+
+  plugins: [
+    // https://github.com/babel/babel/issues/10261
+    ['@babel/plugin-transform-runtime', {
+      version: require('@babel/helpers/package.json').version,
+    }],
+  ],
+};
